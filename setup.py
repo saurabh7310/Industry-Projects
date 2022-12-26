b07bae629162039b3513f6d4551b573cbf35f753
saurabh7310 <saurabh.sharma7310@gmail.com>
2022-12-26 14:31:52 +0530

    Updated

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+from typing import List
+
+REQUIREMENT_FILE_NAME = 'requirements.txt'
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements()->List[str]:
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requiremnt_file.readline()
+    requirement_list = [requirement_name.replace("\n", "") for requirement_name in requiremnt_list]
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list
+
+
+setup(
+    name = 'sensor',
+    version = '0.0.1',
+    author = 'Saurabh Sharma',
+    author_email = 'saurabh.sharma7310@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements(), 
+)
