ba5db4e84b39f7fa6d53f21931e7df0d2c1b74f1
saurabh7310 <saurabh.sharma7310@gmail.com>
2022-12-28 01:17:47 +0530

    Updating for Next day

--- sensor/logger.py
+++ sensor/logger.py
@@ -3,20 +3,21 @@ import os
 from datetime import datetime
 import os
 
-# Log file name
+#log file name
 LOG_FILE_NAME = f"{datetime.now().strftime('%m%d%Y__%H%M%S')}.log"
 
-# Log directory
-LOG_FILE_DIR = os.path.join(os.getcwd(), "logs")
+#log directory
+LOG_FILE_DIR = os.path.join(os.getcwd(),"logs")
 
-# Create folders if not available
-os.makedirs(LOG_FILE_DIR, exist_ok=True)
+#create folder if not available
+os.makedirs(LOG_FILE_DIR,exist_ok=True)
 
-# Log file path 
-LOG_FILE_PATH = os.path.join(LOG_FILE_DIR, LOG_FILE_NAME)
+#log file path
+
+LOG_FILE_PATH = os.path.join(LOG_FILE_DIR,LOG_FILE_NAME)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
+    level=logging.DEBUG,
 )
