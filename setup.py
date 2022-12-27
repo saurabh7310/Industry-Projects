3824cc35485edf4c49db62134fa0579f88094132
saurabh7310 <saurabh.sharma7310@gmail.com>
2022-12-27 22:05:21 +0530

    Updated

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ HYPHEN_E_DOT = "-e ."
 
 def get_requirements()->List[str]:
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        requirement_list = requiremnt_file.readline()
-    requirement_list = [requirement_name.replace("\n", "") for requirement_name in requiremnt_list]
+        requirement_list = requirement_file.readline()
+    requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
     if HYPHEN_E_DOT in requirement_list:
         requirement_list.remove(HYPHEN_E_DOT)
     return requirement_list
